a08826cb7d975125f6d280c5a89511ac5a372b9e
Rohit Nigam <rohit.nigam@cerner.com>
2015-11-15 17:23:42 +0530

    tabbed menu work. icon fixes

--- js/script.js
+++ js/script.js
@@ -47,4 +47,24 @@ $(document).on("scroll", function(){
 			}
 		});
 
+    $('#horizontalTab').easyResponsiveTabs({
+
+    //Types: default, vertical, accordion
+    type: 'default',
+
+    //auto or any width like 600px
+    width: 'auto',
+
+    // 100% fit in a container
+    fit: true,
+
+    // Start closed if in accordion view
+    closed: 'accordion',
+
+    // Callback function if tab is switched
+    activate: function(){}
+
+    });
+
+
 });
